5a66ab872e38acebd5eda27edf34761455de5538
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-24 22:34:43 -0500

    init commit

---
+++ .shadow-cljs/builds/app/dev/goog-js/shadow.module.app.append.js
@@ -0,0 +1,1 @@
+["^ ","~:resource-id",["~:shadow.build.modules/append","~:app"],"~:source","\nportfolio.core.init();\nshadow.cljs.devtools.client.browser.module_loaded('app');\n","~:js","\nportfolio.core.init();\nshadow.cljs.devtools.client.browser.module_loaded('app');\n"]
---
+++ src/cljs/portfolio/core.cljs
@@ -0,0 +1,25 @@
+(ns portfolio.core
+  (:require
+   [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
+   [portfolio.events :as events]
+   [portfolio.routes :as routes]
+   [portfolio.views :as views]
+   [portfolio.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app")))
+
+(defn init []
+  (routes/app-routes)
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
